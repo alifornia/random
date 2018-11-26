2cda8f7923ba0c9daeea9aae85b0d209b1d15169
abbasinasb <a.abbasinasab@gmail.com>
2018-11-25 21:25:54 -0800

    OO approval functions

--- python-basics/main.cpp
+++ python-basics/main.cpp
@@ -15,16 +15,20 @@ ostream& operator<<(ostream& os, const vector<T>& v){copy(v.begin(), v.end(), os
 
 class TwitterApprovalSystem{
 private:
+    string repo_path;
+    vector<string> approvers, changed_files, required_approvers, dependencies;
     void process_program_options(const int ac, const char *const av[]);
+    void read_file(string path);
+    void get_required_approvers();
 public:
     TwitterApprovalSystem(int argc, char** argv){
         process_program_options(argc, argv);
     }
+    bool is_approved();
 };
 
 void TwitterApprovalSystem::process_program_options(const int ac, const char *const av[]){
     po::options_description desc("Usage:");
-    vector<string> approvers, changed_files;
     desc.add_options()
         ("approvers,a", po::value(&approvers)->multitoken(), "List of approvers") //("help,h", "produce help message")
         ("changed-files,c", po::value(&changed_files)->multitoken(), "List of changed files");
@@ -36,8 +40,23 @@ void TwitterApprovalSystem::process_program_options(const int ac, const char *co
 
 }
 
+void TwitterApprovalSystem::get_required_approvers(){
+    while(!changed_files.empty()){
+        
+        cout << "DEBUD: not empty" << endl;
+        return;
+    }
+}
+
+bool TwitterApprovalSystem::is_approved(){
+    if(required_approvers == approvers) //handle order!!!!
+        return true;
+    return false;
+}
 int main(int argc, char** argv){
     TwitterApprovalSystem app(argc, argv);
-
+    if(app.is_approved())
+        cout << "Approved" << endl;
+    else cout << "Insufficient approvals" << endl;
     return 0;
 }
