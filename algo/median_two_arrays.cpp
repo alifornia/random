6a3542282a73329c517499b70c319d3aafb0d351
abbasinasb <a.abbasinasab@gmail.com>
2019-01-23 23:39:51 -0800

    Median of two sorted arrays of different sizes - odd an even

--- algo/median_two_arrays.cpp
+++ algo/median_two_arrays.cpp
@@ -12,44 +12,46 @@ ostream &operator<<(ostream &os, const vector<T> &v)
     return os;
 }
 
-int findMedian(vector<int> &v1, vector<int> &v2)
+double findMedian(vector<int> &v1, vector<int> &v2)
 {
     int i = 0;
     int j = 0;
     int v1s = v1.size();
     int v2s = v2.size();
     int vs = (v1s + v2s);
-    int med = -1;
-
-    if (vs % 2 == 1)
+    int med[2] = {0, 0};
+    int m = 0;
+    for (int k = 0; k < vs / 2 + 1; k++)
     {
-        for (int k = 0; k < vs / 2 + 1; k++)
+        cout << "k=" << k << ": ";
+        if (i != v1s && j != v2s)
         {
-            cout << "k=" << k << ": ";
-            if (i != v1s && j != v2s)
-            {
-                cout << "1(" << i << "," << j << ")" << endl;
-                med = (v1[i] < v2[j]) ? v1[i++] : v2[j++];
-            }
-            else if (i == v1s)
-            {
-                cout << "2(" << i << "," << j << ")" << endl;
-                med = v2[j++];
-            }
-            else if (j == v2s)
-            {
-                cout << "3(" << i << "," << j << ")" << endl;
-                med = v1[i++];
-            }
+            cout << "1(" << i << "," << j << ") --> ";
+            if (v1[i] < v2[j])
+                m = v1[i++];
+            else
+                m = v2[j++];
+            cout << "1(" << i << "," << j << ") = " << m << endl;
         }
+        else if (i == v1s)
+        {
+            cout << "2(" << i << "," << j << ") --> ";
+            m = v2[j++];
+            cout << "2(" << i << "," << j << ") = " << m << endl;
+        }
+        else if (j == v2s)
+        {
+            cout << "3(" << i << "," << j << ") --> ";
+            m = v1[i++];
+            cout << "3(" << i << "," << j << ") = " << m << endl;
+        }
+        med[0] = med[1];
+        med[1] = m;
     }
+    if (vs % 2 == 1) //odd
+        return double(max(med[0], med[1]));
     else
-    {
-        for (int k = 0; k < vs / 2; k++)
-        {
-        }
-    }
-    return med;
+        return double(med[0] + med[1]) / 2.0;
 }
 int main()
 {
