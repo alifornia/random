17ce213c44d87ad272c51700a7b40620bf91e5e6
abbasinasb <a.abbasinasab@gmail.com>
2019-02-01 20:46:13 -0800

    complicated input

--- python-basics/genapsys.py
+++ python-basics/genapsys.py
@@ -12,15 +12,14 @@ def sig(prem, template):
     while(l-i and ll-j):
         sig = 0
         print template[i]+prem[j]
-        if template[i]+prem[j] not in dna_match:
-            print 'not'
+        # if template[i]+prem[j] not in dna_match:
+        # print 'not'
         if template[i]+prem[j] in dna_match:
             print 'if'
             sig = 1
             while(template[i+1] and template[i] == template[i+1]):
                 i += 1
                 sig += 1
-
         j += 1
         i += 1
         print('sig =', sig)
@@ -28,8 +27,8 @@ def sig(prem, template):
 
 
 def main():
-    prem = 'ACTGGGGCTA'
-    template = 'TCCCTTTACCGATCA'
+    prem = 'ACTGAAGGACTG'
+    template = 'TCAATTTTTTGCAGGCAT'
     print 'working'
     sig(prem, template)
 
