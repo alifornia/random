f7faa95107d5db522eab19fd1f0a3f179e658eb9
abbasinasb <a.abbasinasab@gmail.com>
2019-02-01 20:20:09 -0800

    CG AT

---
+++ python-basics/genapsys.py
@@ -0,0 +1,17 @@
+# C-G
+# A-T
+
+
+def sig(prem, template):
+    return
+
+
+def main():
+    prem = 'AAA'
+    template = 'CCC'
+    print 'working'
+    sig(prem, template)
+
+
+if __name__ == "__main__":
+    main()
