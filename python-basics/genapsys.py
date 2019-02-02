59c9d8c62875fcd7127b81ed288ce3fb3319313b
abbasinasb <a.abbasinasab@gmail.com>
2019-02-01 20:22:27 -0800

    CG AT

--- python-basics/genapsys.py
+++ python-basics/genapsys.py
@@ -3,12 +3,23 @@
 
 
 def sig(prem, template):
-    return
+    l = len(template)
+    ll = len(prem)
+    i = j = 0
+    sig = 0
+    while(l-i and ll-j):
+        sig = 0
+        print template[i]+prem[j]
+
+        j += 1
+        i += 1
+        print('sig =', sig)
+    return sig
 
 
 def main():
-    prem = 'AAA'
-    template = 'CCC'
+    prem = 'ACTGGGGCTA'
+    template = 'CCCTTTACCGATCA'
     print 'working'
     sig(prem, template)
 
