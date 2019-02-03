821b7aa86838f60ef934a70c850aee851c8dc957
abbasinasb <a.abbasinasab@gmail.com>
2019-02-03 06:18:34 -0800

    fixed merged node and Node

--- algo/graph.cpp
+++ algo/graph.cpp
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <iostream>
 #include <vector>
@@ -13,20 +7,6 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 using namespace std;
 
-<<<<<<< HEAD
-class Node{
-public:
-    int id;
-    Node(int id_):id(id_){}
-    vector<Node*> childs;
-};
-
-class Graph{
-public:
-    int v;
-    int e;
-    Graph():v(0),e(0){}
-=======
 struct node
 {
     int id;
@@ -48,30 +28,11 @@ class Graph
     int v;
     int e;
     Graph() : v(0), e(0) {}
->>>>>>> refs/remotes/origin/master
     vector<Node *> roots;
     void BFSi();
     void BFSr();
 };
 
-<<<<<<< HEAD
-void Graph::BFSi(){
-    for(auto & root : roots){
-        queue<Node*> q;
-        vector<bool> visited(v, false);
-
-        q.push(root);
-        visited[root->id]=true;
-
-        while(!q.empty()){
-            root = q.front();
-            cout << "root: " << root->id << endl;
-            q.pop();
-            for(auto & child : root->childs){
-                cout << "child: " << child->id << endl;
-                if(!visited[child->id]){
-                    visited[child->id]=true;
-=======
 void Graph::BFSi()
 {
     for (auto &root : roots)
@@ -93,7 +54,6 @@ void Graph::BFSi()
                 if (!visited[child->id])
                 {
                     visited[child->id] = true;
->>>>>>> refs/remotes/origin/master
                     q.push(child);
                 }
             }
@@ -110,32 +70,14 @@ void Graph::BFSi()
 
 vector<vector<int>> g;
 
-<<<<<<< HEAD
-void BFS(vector<vector<int>> &g){
-
-=======
 void BFS(vector<vector<int>> &g)
 {
->>>>>>> refs/remotes/origin/master
 }
 
 int main()
 {
     Node node0(0), node1(1), node2(2), node3(3), node4(4);
 
-<<<<<<< HEAD
-    node0.childs={&node1, &node2};
-    node1.childs={&node0, &node3, &node4};
-    node2.childs={&node0, &node3};
-    node3.childs={&node1, &node2, &node4};
-    node4.childs={&node1, &node3};
-
-    Graph G;
-    G.v=5;
-    G.e=6;
-
-    G.roots={&node0};
-=======
     node0.childs = {&node1, &node2};
     node1.childs = {&node0, &node3, &node4};
     node2.childs = {&node0, &node3};
@@ -147,7 +89,7 @@ int main()
     G.e = 6;
 
     G.roots = {&node0};
->>>>>>> refs/remotes/origin/master
+
     G.BFSi();
 
     return 0;
--- python-basics/genapsys.py
+++ python-basics/genapsys.py
@@ -10,7 +10,9 @@ def sig(prem, template, threshold):
     i = j = 0
     while(l-i and ll-j):
         sig = 0
+        print prem[j], template[i]
         if template[i]+prem[j] in dna_match:
+            print template[i]+prem[j]
             sig = 1
             while(template[i+1] and template[i] == template[i+1] and sig < threshold):
                 i += 1
@@ -23,7 +25,7 @@ def sig(prem, template, threshold):
 
 def main():
     prem = "ACTGAAGGACTG"
-    template = "TCAATTTTTTGCAGGCAT"
+    template = "TCAAAAATTTTTTGCAGGCAT"
     threshold = 3
     sig(prem, template, threshold)
 
