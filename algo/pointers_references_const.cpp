5bbf5cd1686cb0ccfbaf261d6fc365df948a7081
abbasinasb <a.abbasinasab@gmail.com>
2019-02-03 18:26:36 -0800

    an old note on how references look like memory wise in C++

--- algo/pointers_references_const.cpp
+++ algo/pointers_references_const.cpp
@@ -5,45 +5,30 @@ using namespace std;
 
 int main()
 {
-    int a = -1;
-    cout << "a" << endl;
-    cout << a << endl;
-    cout << &a << endl
-         << endl;
-    const int ca = -2;
-    cout << "ca" << endl;
-    cout << ca << endl;
-    cout << &ca << endl
-         << endl;
+    int i = -1;
+    const int ci = -2;
+    int *pi;
+    pi = &i;
+    //pi = &ci; error
+    int *qi;
+    qi = pi;
 
-    int *p;
-    p = &a;
-    //p = &ca; error
-    cout << "p" << endl;
-    cout << p << endl;
-    cout << *p << endl;
-    cout << &p << endl
-         << endl;
+    const int *pci; //ptr to cns int, same as int const *
+    pci = pi;
 
-    int *q;
-    q = p;
-    cout << "q" << endl;
-    cout << q << endl;
-    cout << *q << endl;
-    cout << &q << endl
-         << endl;
+    //int *const cp; error must be initilized
+    int *const cpi = &i;
+    //cpi = &i; error cuz it's const
+    *cpi = i;
 
-    //ptr to cns int
-    //same as int const *
-    const int *pc;
-    pc = p;
-    cout << "pc" << endl;
-    cout << pc << endl;
-    cout << *pc << endl;
-    cout << &pc << endl
-         << endl;
+    const int *const cpci = &i;
+    //*cpci = i; error
+    //cpci = &i; error
 
-    int *const cp;
-    cp = &a;
+    //int &r; error must be initilized
+    int &ri = i;
+    cout << &ri << endl;
+    cout << &ri << endl;
+
     return 0;
 }
