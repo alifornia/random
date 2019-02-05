1307ed8b4664287eaa8e44ebed2871e0762f3c98
abbasinasb <a.abbasinasab@gmail.com>
2019-02-05 01:00:18 -0800

    bst insrt, search using unqptr

--- algo/bst.cpp
+++ algo/bst.cpp
@@ -14,54 +14,76 @@ struct Node
     unique_ptr<Node> right;
 };
 
-//const if not changing
-void display(const unique_ptr<Node> &root)
+class BST
 {
-    if (root != nullptr)
+  public:
+    void insert(unique_ptr<Node> &root, int data)
     {
-        display(root->left);
-        cout << root->data << " ";
-        display(root->right);
+        if (root == nullptr)
+            root = move(create(data)); //avoid creating another copy of obejct READ ON: //http://thbecker.net/articles/rvalue_references/section_01.html
+        else if (data < root->data)
+            insert(root->left, data);
+        else
+            insert(root->right, data);
     }
-}
+    void remove(unique_ptr<Node> &root, int data)
+    {
+        if (root == nullptr)
+            root = move(create(data)); //avoid creating another copy of obejct READ ON: //http://thbecker.net/articles/rvalue_references/section_01.html
+        else if (data < root->data)
+            insert(root->left, data);
+        else
+            insert(root->right, data);
+    }
+    unique_ptr<Node> search(unique_ptr<Node> &root, int key)
+    {
+        if (root == nullptr || root->data == key)
+            return move(root);
+        else if (key < root->data)
+            return search(root->left, key);
+        else
+            return search(root->right, key);
+    }
+    void display(const unique_ptr<Node> &root) //const if not changing
+    {
+        if (root)
+        {
+            display(root->left);
+            cout << root->data << " ";
+            display(root->right);
+        }
+    }
 
-unique_ptr<Node> create(int data)
-{
-    auto node = make_unique<Node>();
-    node->data = data;
-    return move(node); //https://stackoverflow.com/questions/3413470/what-is-stdmove-and-when-should-it-be-used
-}
-
-void insert(unique_ptr<Node> &root, int data)
-{
-    if (root == nullptr)
-        root = move(create(data)); //avoid creating another copy of obejct READ ON: //http://thbecker.net/articles/rvalue_references/section_01.html
-    else if (data < root->data)
-        insert(root->left, data);
-    else
-        insert(root->right, data);
-}
+  private:
+    unique_ptr<Node> create(int data)
+    {
+        auto node = make_unique<Node>();
+        node->data = data;
+        return move(node); //https://stackoverflow.com/questions/3413470/what-is-stdmove-and-when-should-it-be-used
+    }
+};
 
 int main()
 {
-    unique_ptr<Node> root = nullptr;
-    vector<int> nodes;
-
-    string line;
+    string line = "22 -1 3 100 7 40 -7 6 -9 -30 20 21 19";
     int value;
-
-    getline(cin, line);
-
+    // getline(cin, line);
     stringstream split(line);
-    while (split >> value)
-        nodes.push_back(value);
 
-    for (int i = 0; i < nodes.size(); i++)
-        insert(root, nodes[i]);
-    display(root);
+    BST bst;
+    unique_ptr<Node> root = nullptr;
+    while (split >> value)
+        bst.insert(root, value);
+    cout << "root: " << root->data << endl;
+
+    bst.display(root);
+    cout << endl;
+
+    unique_ptr<Node> term = bst.search(root, 5);
+    term ? cout << "found: " << term->data << endl : cout << "not found" << endl;
+
     return 0;
 }
-//5 4 6 3 2
-//7 4 8 3 5 2
-//2 -1 3 100 7 40 -7 -9 -30 20 21
+
+//22 -1 3 100 7 40 -7 6 -9 -30 20 21 19
 //1 3 6 8 10 14 13 4 7
