1b52f5949f83df85b7d897969ac16ab2f861ad04
abbasinasb <a.abbasinasab@gmail.com>
2019-02-10 00:24:06 -0800

    shallow vs deep copy constructor

--- python-basics/constructors.cpp
+++ python-basics/constructors.cpp
@@ -8,21 +8,39 @@ class X
   public:
     int i;
     int *pi;
-    X() : pi(new int) {}
-    X(int i, int *pi) : i(i), pi(pi) {}
-    //X(const X &copy) : i(copy.i), pi(copy.pi) {} // shallow copy
-    X(const X &copy) : i(copy.i), pi(new int(*copy.pi)) {} // deep copy
+    X() : pi(new int) {}                //user defined default constructor
+    X(int i, int *pi) : i(i), pi(pi) {} //user defined default constructor with initializer
+    //X(const X &copy) : i(copy.i), pi(copy.pi) {} // user defined copy constructor shallow copy
+    X(const X &copy) : i(copy.i), pi(new int(*copy.pi)) {} // user defined copy constructor shallow copy
 };
 
 int main()
 {
 
-    int p[] = {1, -1, 0};
+    int p[] = {2, 3};
+    X x0;
+    x0.pi = p;
     X x1(1, p);
-    X x2(x1);
-    x2.pi[0] = 2;
-    cout << *x1.pi << endl;
-    cout << *x2.pi << endl;
+    X x2(x1);  //cc
+    X x3 = x1; //cc
+    X x4;
+    x4 = x1; //as
+    x2.pi[0] = -2;
+    x2.i = 2;
+    x1.i = -100;
+
+    cout << x0.i << " - " << *x0.pi << endl;
+    cout << x1.i << " - " << *x1.pi << endl;
+    cout << x2.i << " - " << *x2.pi << endl;
+    cout << x3.i << " - " << *x3.pi << endl;
+    cout << x4.i << " - " << *x4.pi << endl;
+
+    //default constructor (default default or user-defined default!):
+    //discussing shallow or deep does not even make sense :D no copy
+
+    //copy constructor
+    //i member of class is always deep copied (different)
+    //pi member of class is always shallow copied (same) unless a user defined copy constructor with deep copy implemented
 
     return 0;
 }
