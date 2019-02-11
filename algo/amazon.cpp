bcc629ab09dc119dd2e1258a323f9b3752a11d76
abbasinasb <a.abbasinasab@gmail.com>
2019-02-11 01:13:49 -0800

    short length

--- algo/amazon.cpp
+++ algo/amazon.cpp
@@ -35,7 +35,7 @@ int bfs_s(pair<int, int> s, const vector<vector<int>> &m)
     pair<int, int> cell;
     vector<vector<bool>> visited(m.size(), vector<bool>(m[0].size(), 0));
 
-    int l = 0;
+    int len = 0;
 
     vector<int> shortest_path;
     queue<pair<int, int>> qcell;
@@ -49,53 +49,57 @@ int bfs_s(pair<int, int> s, const vector<vector<int>> &m)
     {
         cell = qcell.front();
         qcell.pop();
-        //l = qlen.front();
-        //qlen.pop();
+        len = qlen.front();
+        qlen.pop();
         cout << "w (" << cell.first << "," << cell.second << ")" << endl;
         int l = isValid(cell, {0, -1}, m);
         if (l > 0 && !visited[cell.first][cell.second - 1])
         {
             if (l == 9)
-                cout << "yay" << endl;
-            cout << "1 (" << cell.first << "," << cell.second - 1 << ")" << endl;
+                return len + 1;
+            cout << "1 (" << cell.first << "," << cell.second - 1 << ") = " << len << endl;
             visited[cell.first][cell.second - 1] = true;
             qcell.push({cell.first, cell.second - 1});
+            qlen.push(len + 1);
         }
         int u = isValid(cell, {-1, 0}, m);
         if (u > 0 && !visited[cell.first - 1][cell.second])
         {
             if (u == 9)
-                cout << "yay" << endl;
-            cout << "2 (" << cell.first - 1 << "," << cell.second << ")" << endl;
+                return len + 1;
+            cout << "2 (" << cell.first - 1 << "," << cell.second << ") = " << len << endl;
             visited[cell.first - 1][cell.second] = true;
             qcell.push({cell.first - 1, cell.second});
+            qlen.push(len + 1);
         }
         int r = isValid(cell, {0, +1}, m);
         if (r > 0 && !visited[cell.first][cell.second + 1])
         {
             if (r == 9)
-                cout << "yay" << endl;
-            cout << "3 (" << cell.first << "," << cell.second + 1 << ")" << endl;
+                return len + 1;
+            cout << "3 (" << cell.first << "," << cell.second + 1 << ") = " << len << endl;
             visited[cell.first][cell.second + 1] = true;
             qcell.push({cell.first, cell.second + 1});
+            qlen.push(len + 1);
         }
         int d = isValid(cell, {+1, 0}, m);
         if (d > 0 && !visited[cell.first + 1][cell.second])
         {
             if (d == 9)
-                cout << "yay" << endl;
-            cout << "4 (" << cell.first + 1 << "," << cell.second << ")" << endl;
+                return len + 1;
+            cout << "4 (" << cell.first + 1 << "," << cell.second << ") = " << len << endl;
             visited[cell.first + 1][cell.second] = true;
             qcell.push({cell.first + 1, cell.second});
+            qlen.push(len + 1);
         }
     }
-    return 0;
+    return -1;
 }
 
 int main()
 {
     vector<vector<int>> m = {{1, 1, 1, 1, 0}, {2, 0, 0, 1, 1}, {1, 1, 1, 1, 1}, {0, 1, 9, 1, 0}};
     print(m);
-    bfs_s({1, 0}, m);
+    cout << bfs_s({1, 0}, m) << endl;
     return 0;
 }
