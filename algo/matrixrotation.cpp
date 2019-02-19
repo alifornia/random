364ca6c5f5ecd72c90b2dd56ac88468eb68c3f25
abbasinasb <a.abbasinasab@gmail.com>
2019-02-19 02:56:29 -0800

    better indexing for col and row, improved readibilitygit add *

--- algo/matrixrotation.cpp
+++ algo/matrixrotation.cpp
@@ -22,42 +22,47 @@ int main()
         while (split >> value)
             v.back().push_back(value);
     }
-    int col = 5;
-    int row = 4;
-    int r = 7;
+    int col, row, r;
+    col = v[0][0];
+    row = v[0][1];
+    r = v[0][2];
+    v.erase(v.begin(), v.begin() + 1);
 
     int rings = min(col, row) / 2;
-    cout << "rings: " << rings << endl;
     int temp_element;
-    for (int ring = 0; ring < rings; ring++)
+    for (int i = 0; i < rings; i++)
     {
         //avoid 360 rotation for each ring
-        int rotations = r % (2 * (col + row - 4 * ring) - 4);
+        int rotations = r % (2 * (col + row - 4 * i) - 4);
         for (int rot = 0; rot < rotations; rot++)
         {
-            for (int urow = ring; urow < row - ring - 1; urow++)
+            //upper row
+            for (int j = i; j < row - i - 1; j++)
             {
-                temp_element = v[ring][urow];
-                v[ring][urow] = v[ring][urow + 1];
-                v[ring][ring + 1] = temp_element;
+                temp_element = v[i][j];
+                v[i][j] = v[i][j + 1];
+                v[i][j + 1] = temp_element;
             }
-            for (int rcol = ring; rcol < col - ring - 1; rcol++)
+            //right column
+            for (int j = i; j < col - i - 1; j++)
             {
-                temp_element = v[rcol][row - ring - 1];
-                v[rcol][row - ring - 1] = v[rcol + 1][row - ring - 1];
-                v[rcol + 1][row - ring - 1] = temp_element;
+                temp_element = v[j][row - i - 1];
+                v[j][row - i - 1] = v[j + 1][row - i - 1];
+                v[j + 1][row - i - 1] = temp_element;
             }
-            for (int brow = row - ring - 1; brow > ring; brow--)
+            //bottom row
+            for (int j = row - i - 1; j > i; j--)
             {
-                temp_element = v[ring][brow];
-                v[ring][brow] = v[col - ring - 1][brow - 1];
-                v[col - ring - 1][brow - 1] = temp_element;
+                temp_element = v[col - i - 1][j];
+                v[col - i - 1][j] = v[col - i - 1][j - 1];
+                v[col - i - 1][j - 1] = temp_element;
             }
-            for (int lcol = col - ring - 1; lcol > ring; lcol--)
+            //left column
+            for (int j = col - i - 1; j > i + 1; j--)
             {
-                temp_element = v[lcol][ring];
-                v[lcol][ring] = v[lcol - 1][ring];
-                v[lcol - 1][ring] = temp_element;
+                temp_element = v[j][i];
+                v[j][i] = v[j - 1][i];
+                v[j - 1][i] = temp_element;
             }
         }
     }
