fc3f6927f547db9ea6b8cb9833c02c2c9f5f8646
abbasinasb <a.abbasinasab@gmail.com>
2019-02-18 20:12:52 -0800

    read input added

--- algo/matrixrotation.cpp
+++ algo/matrixrotation.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
-
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{ // print util CMT laterrr
+    copy(v.begin(), v.end(), ostream_iterator<T>(cout, " "));
+    return os;
+}
 
 int main()
 {
+    vector<vector<int>> v;
+    string line;
+    int value;
+    while (getline(cin, line))
+    {
+        v.push_back(vector<int>());
+        stringstream split(line);
+        while (split >> value)
+            v.back().push_back(value);
+    }
 
     return 0;
 }
